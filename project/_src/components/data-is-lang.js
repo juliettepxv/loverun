c6117a5e339eb6af1f9ed6c457a613937cb36171
david <d.marsalone@gmail.com>
2018-08-06 08:09:47 +0200

    oups

--- project/_src/components/data-is-lang.js
+++ project/_src/components/data-is-lang.js
@@ -3,6 +3,5 @@ $body.on("mousedown","[data-is-lang]:not(.active)",function(e){
     e.stopPropagation();
     let lang=$(this).attr("data-is-lang");
     let url=PovHistory.currentPageInfo.languagesUrls[lang];
-    alert(url);
     document.location=url;
 });
