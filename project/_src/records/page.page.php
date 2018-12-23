dffa35036be6d7d98f98ed717b667a45704cbf9e
davidmars <d.marsalone@gmail.com>
2018-12-23 09:18:44 +0100

    page layout + bdd

--- project/_src/records/page.page.php
+++ project/_src/records/page.page.php
@@ -3,17 +3,7 @@
 use Classiq\Models\Page;
 $view->inside("layout/layout",$vv);
 ?>
-<div class="py-big">
-
-        <div class="container">
-            <?=$vv->wysiwyg()
-                ->field("name_lang")
-                ->string(\Pov\Utils\StringUtils::FORMAT_NO_HTML_SINGLE_LINE)
-                ->setDefaultValue($vv->name)
-                ->htmlTag("h1")
-                ->addClass("mb-big")
-            ?>
-        </div>
+<div class="pb-big">
 
         <?=$vv->wysiwyg()->field("blocks")
             ->listJson(site()->blocksList)
