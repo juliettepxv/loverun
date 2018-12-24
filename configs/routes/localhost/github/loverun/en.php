49f267a26501b2e49ee9a1e3c70bd66f11a1c5a1
davidmars <d.marsalone@gmail.com>
2018-12-24 07:31:35 +0100

    configs localhost et bdd

---
+++ configs/routes/localhost/github/loverun/en.php
@@ -0,0 +1,30 @@
+<?php
+the()->fmkHttpRoot="/github/loverun";// /github/classiq-startup
+the()->configProjectUrl=new \Pov\Configs\ProjectUrl("localhost/github/loverun/en"); // localhost/github/classiq-startup/fr
+the()->fileSystem=new \Pov\Configs\FileSystem("project");
+the()->configProjectUrl->seoActive=true;
+the()->boot->loadProject("project");
+the()->project->langCode="en";
+the()->project->languages=["fr","en"];
+the()->project->languagesUrls=[
+    "fr"=>"http://localhost/github/loverun/fr",
+    "en"=>"http://localhost/github/loverun/en"
+];
+//force https?
+the()->configProjectUrl->forceHttps=false;
+// Traductions ui.
+// https://docs.google.com/spreadsheets/d/1kefhXt0Z7g0P2cGItQK4k_hqhSoFTMaJyvzNYjKeqw8
+// Copiez-collez ce google Sheet pour pouvoir le modifier puis modifiez l'url. Vous pouvez bien entendu utiliser un csv en local mais c'est moins pratique :)
+the()->project->config_translations_csv_url="https://docs.google.com/spreadsheets/d/1kefhXt0Z7g0P2cGItQK4k_hqhSoFTMaJyvzNYjKeqw8/export?gid=0&format=csv";
+the()->project->config_translations_debug=true; //quand true recharge à chaque fois le CSV
+
+//config options
+
+//Google Map etc...
+//site()->googleApiKey="xxxxx";
+
+//Google webmaster tools
+//site()->googleSiteVerification="xxxxx";
+
+//Google analytics
+//site()->googleAnalyticsId="UA-xxxxxx";
