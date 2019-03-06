64f4716e8c96b819ff3a4f4edfb611046e0b1589
david <d.marsalone@gmail.com>
2019-03-06 11:29:21 +0100

    config peru.love-run.com redirection

--- configs/routes/peru.love-run.com.php
+++ configs/routes/peru.love-run.com.php
@@ -1,4 +1,11 @@
 <?php
+header('Location: https://love-run.com');
+die();
+?>
+
+<?php
+
+
 the()->fmkHttpRoot="";// /github/classiq-startup
 the()->configProjectUrl=new \Pov\Configs\ProjectUrl("love-run.com"); // localhost/github/classiq-startup/fr
 the()->fileSystem=new \Pov\Configs\FileSystem("project");
