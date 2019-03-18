efcfc6cd7303bd348e74dde77e82231909aa6734
davidmars <d.marsalone@gmail.com>
2019-03-18 15:57:17 +0100

    clean code inutile

--- project/_src/blocks/block-faq/faq.php
+++ project/_src/blocks/block-faq/faq.php
@@ -1,8 +1,5 @@
 <?php
 /** @var Classiq\Models\JsonModels\ListItem $vv */
-$cssBg=$vv->getData("color","bg-white");
-$style=$vv->getData("style","icones-texte");
-$deco=$vv->getData("deco",null)?"deco":"";
 ?>
 <div <?= $vv->wysiwyg()->attr() ?> class="block block-faq">
     <div class="deco"></div>
@@ -11,13 +8,13 @@ $deco=$vv->getData("deco",null)?"deco":"";
             <?=$vv->wysiwyg()
                 ->field("title_lang")
                 ->string()
-                ->setPlaceholder("Titre ici")
+                ->setPlaceholder(trad("Titre ici"))
                 ->htmlTag("h2")?>
 
             <?=$vv->wysiwyg()->field("items")
                 ->listJson("blocks/block-faq/faq-item")
                 ->contextMenuPosition(POSITION_TOP_RIGHT)
-                ->blockPickerEmptyMessage("Ajoutez des questions")
+                ->blockPickerEmptyMessage(trad("Ajoutez des questions"))
                 ->contextMenuSize(SIZE_SMALL)
                 ->htmlTag("div")
                 ->addClass("list")
